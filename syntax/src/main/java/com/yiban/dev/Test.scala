85c40793249407cf13efa0facc840a94ea4bef19
基础组-段巍巍 <dww@yiban.cn>
2018-08-01 17:11:18 +0800

    up

--- syntax/src/main/java/com/yiban/dev/Test.scala
+++ syntax/src/main/java/com/yiban/dev/Test.scala
@@ -22,11 +22,12 @@ object Test {
     println(a)
   }
 
-  def testRequires(i:Int) = {
+  def testRequires(i: Int) = {
     /**
       * Exception in thread "main" java.lang.IllegalArgumentException: requirement failed: i:-1 must be non negative
       */
-    require(i>0,s"i:$i must be non negative")
+    require(i > 0, s"i:$i must be non negative")
+
     /**
       * Exception in thread "main" java.lang.AssertionError: assertion failed: i:-1 must be non negative
       */
@@ -36,6 +37,7 @@ object Test {
       */
     println("sssss")
   }
+
 
   def main(args: Array[String]): Unit = {
     val map = Map("one" -> 1, "two" -> 2, "three" -> 3)
@@ -61,14 +63,64 @@ object Test {
       case ex: ArithmeticException => println(ex.getMessage)
     }
 
-    case class A(var a:String)
+    case class A(var a: String)
     val a1 = A("aa")
     val b1 = A.apply("bb")
     println(a1)
     println(b1)
 
-    var a : Char = '中'
+    var a: Char = '中'
 
-    testRequires(-1)
+    //    testRequires(-1)
+
+
+    def seqno(m: Int, n: Int): Int = {
+      val s = "the max between m and n is =%d,%d"
+      println(s.format(m, n))
+      return math.max(m,n)
+    }
+    def combine(m: Int, n: Int): Int = {
+      val s = "com_exp=%d+%d"
+      println(s.format(m, n))
+      return m + n
+    }
+
+    val d = List(1, 2, 3, 4)
+//        val e1 = d.par.aggregate(5)(seqno, combine)
+    val e = d.aggregate(5)(seqno, combine)
+    println("e:" + e)
+
+    val b = List(1, 2, 3, 4)
+
+
+    val res = aggregate(b)(5)(math.max(_, _), _ + _)
+    println(res)
+
+    def x(x1: Int,x2:Int): Int = {
+      return math.max(x1,x2)
+    }
+
+    def y(y1:String):String = {
+      println(s"$y1")
+      y1
+    }
+    println("=============================")
+    def test111(a: (Int,Int) => Int)(b: String => String) = {
+      a
+      b("aaa")
+    }
+    test111(x)(y)
   }
+
+  def foldLeft[Int](a: List[Int])(z: Int)(op: (Int, Int) => Int): Int = {
+    var result = z
+    a foreach (x => result = op(result, x))
+    result
+  }
+
+
+  def aggregate[Int](a: List[Int])(z: => Int)(seqop: (Int, Int) => Int, combop: (Int, Int) => Int): Int = foldLeft(a)(z)(seqop)
+
+
+
 }
